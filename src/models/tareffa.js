b829f2bac516b31dd716f48de9b222faae0fd03e
Michael Anderson da Silva <michael.silva@roderjan.com.br>
2025-12-09 15:15:18 -0300

    feat: criação de novo ususario enviado por API

--- src/models/tareffa.js
+++ src/models/tareffa.js
@@ -3,6 +3,7 @@ function getUserLoginTareffa() {
         'TAREFFA_CLIENT_ID': process.env.TAREFFA_CLIENT_ID,
         'TAREFFA_CLIENT_SECRET': process.env.TAREFFA_CLIENT_SECRET,
         'TAREFFA_USERNAME': process.env.TAREFFA_USERNAME,
+        'TAREFFA_PASSWORD': process.env.TAREFFA_PASSWORD,
         'USERLOGIN': process.env.USERLOGIN,
         'PASSWORD': process.env.PASSWORD,
     }
