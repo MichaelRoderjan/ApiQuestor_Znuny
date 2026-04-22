38d3629f080f532b5c0fe281e5f47a8b5f91fb75
Michael Anderson da Silva <michael.silva@roderjan.com.br>
2026-04-22 14:12:58 -0300

    feat liberação de anexo

--- src/app.js
+++ src/app.js
@@ -6,11 +6,9 @@ const app = express();
 
 app.use(cors());
 
-// Lê JSON do corpo da requisição
-app.use(express.json());
-
-// (opcional, mas recomendado) lê application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+// 🔥 AUMENTANDO LIMITE DO PAYLOAD
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 // Suas rotas
 app.use('/', routes);
